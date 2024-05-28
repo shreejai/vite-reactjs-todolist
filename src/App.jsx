ab06e46adba044d5e41e3de3b28744e288ae8476
Shreejai Raj <shreejairaj@gmail.com>
2024-05-29 09:09:33 +1000

    local storage useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
@@ -7,8 +7,13 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [todoValue, setTodoValue] = useState('');
 
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({todos:newList}))
+  }
+
   function handleAddTodos(newTodo){
     const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
@@ -16,6 +21,7 @@ function App() {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index
     })
+    persistData(newTodoList);
     setTodos(newTodoList)
   }
 
@@ -25,6 +31,20 @@ function App() {
     handleDeleteTodos(index);
   }
 
+  useEffect(()=>{
+    //Check if localStorage exists
+    if(!localStorage) {
+      return
+    }
+    let localTodos = localStorage.getItem('todos');
+
+    //Check if existed before
+    if(!localTodos){
+      return
+    }
+      localTodos = JSON.parse(localTodos).todos
+      setTodos(localTodos)
+  },[])
   return (
     <>
       <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos = {handleAddTodos}/>
